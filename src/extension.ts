d6639187d7e68d39868d7f925f22ebc717a7ff5d
Christopher Wells <cwellsny@nycap.rr.com>
2021-05-09 00:48:45 -0700

    Setup language server support

---
+++ src/extension.ts
@@ -0,0 +1,48 @@
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+    let serverExecutable = path.join("/home", "chris", "Code", "quickbms-lsp", "target", "release", "quickbms-lsp");
+
+    let serverOptions: ServerOptions = {
+        run: { command: serverExecutable, args: [], options: null },
+        debug: { command: serverExecutable, args: [], options: null }
+    };
+
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'quickbms' }],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contained in the workspace
+            fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+        }
+    };
+
+    // Create the language client and start the client.
+    client = new LanguageClient(
+        'quickbmsServer',
+        'QuickBMS Language Server',
+        serverOptions,
+        clientOptions
+    );
+
+    // Start the client. This will also launch the server
+    client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
